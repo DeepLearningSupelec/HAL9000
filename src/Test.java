87666c5e512d9086c7e04bb0b3e83921c240652c
fTrestour <fabien.trestour@supelec.fr>
2015-10-08 14:55:24 +0200

    Test
    Création d'une classe Test munie d'un main pour tous les tests.
    Test rapide du bon fonctionnement de ActivationFunction

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,15 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		/* Test Sigmoid */
+		Sigmoid f1 = new Sigmoid();
+		Sigmoid f2 = new Sigmoid(2.);
+		System.out.println(f1.apply(1.));
+		System.out.println(f2.apply(1.));
+		System.out.println(f1.applyDerivative(2.));
+		System.out.println(f2.applyDerivative(2.));
 
 	}
 
