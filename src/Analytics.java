45ebbfb71fba92104aa9e586a4d00e9ae38a6f53
fTrestour <fabien.trestour@supelec.fr>
2015-10-09 15:50:33 +0200

    Summary
    Description

--- src/Analytics.java
+++ src/Analytics.java
@@ -1,9 +1,39 @@
-import com.sun.org.apache.xpath.internal.operations.String;
+import java.io.*;
 
 public class Analytics {
 	
 	/* Attributes */
-	String location;
+	java.lang.String location;
+	java.lang.String name;
+	java.lang.String extension;
+	FileWriter file;
 	
 	
+	/* Methods */
+	//Initialisation des attributs
+	public Analytics(java.lang.String location, java.lang.String name, java.lang.String extension) throws IOException {
+		
+		this.location = location;
+		this.name = name;
+		this.extension = extension;
+		this.file = new FileWriter(location + '\\' + name + '.' + extension);
+		
+	}
+	
+	//Relacher la memoire
+	public void close() throws IOException {
+		this.file.close();
+	}
+	
+	//Ecrire une ligne complete et passer à la ligne
+	public void newLine(java.lang.String name, Double[] data) throws IOException {
+		
+		this.file.write(name + ";");
+		
+		for(int i=0; i<data.length+1; i++) {
+			this.file.write(data[i] + ";");
+		};
+		this.file.write("\n");
+		
+	}	
 }
--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,10 @@
+import java.io.IOException;;
 
 public class Test {
 
-	public static void main(String[] args) {
+	private static Double d[];
+
+	public static void main(String[] args) throws IOException {
 		
 		
 		/* Test Sigmoid */
@@ -17,6 +20,15 @@ public class Test {
 		/*ActivationFunction f = new ActivationFunction(x->x*x, x->2*x);
 		System.out.println(f.apply(3.));
 		System.out.println(f.applyDerivative(3.));*/
+		
+		
+		/* Test Analytics */
+		Analytics test = new Analytics("C:\\Users\\Fabien\\Desktop\\","essai","csv");
+		for(int i=0; i<11; i++){
+			d[i] = (double) i;
+		};
+		test.newLine("bite", d);
+		test.close();
 
 	}
 
