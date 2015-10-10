bd2819f546ee8f1a6659dc0d912f0ecc10c3bcfd
fTrestour <fabien.trestour@supelec.fr>
2015-10-10 18:02:45 +0200

    Back to basics
    Retour à une classe activationFunction abstraite parce que pourquoi
    pas...

--- src/Step.java
+++ src/Step.java
@@ -1,4 +1,14 @@
 
 public class Step extends ActivationFunction {
 
+	@Override
+	public Double apply(Double x) {
+		return (double) ((x<0)?0:1);
+	}
+
+	@Override
+	public Double applyDerivative(Double x) {
+		return null;
+	}
+
 }
