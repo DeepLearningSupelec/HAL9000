feca0140e3ba4bb89816a9b4b47a6d7fa9a9233a
Guriido <christian.hamelain@laposte.net>
2015-10-27 14:37:07 +0100

    test

--- src/Test.java
+++ src/Test.java
@@ -20,7 +20,7 @@ public class Test {
 			//testPerceptron.setInputs(learningDataManager.readImage1D());
 			testPerceptron.setNormalizedInputs(learningDataManager.readImage1D(), 256);
 			testPerceptron.fire();
-			algorithm.launch(testPerceptron, learningRate , currentInput);			
+			//algorithm.launch(testPerceptron, learningRate , currentInput);			
 			double[] outputs=testPerceptron.getOutputs();
 			for(int j =0; j<10;j++){
 				System.out.print(outputs[j]+" ");
