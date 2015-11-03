404ead613bd2523f035c79d2c7fc788ee9fc2aff
Guriido <christian.hamelain@laposte.net>
2015-11-03 10:32:58 +0100

    added constructor for testing Input in Class Input

--- src/Input.java
+++ src/Input.java
@@ -11,13 +11,24 @@ public class Input {
 	
 	//Constructor
 	
-		public Input(int a) throws IOException{
-			MnistManager m = new MnistManager("src/train-images.idx3-ubyte","src/train-labels.idx1-ubyte");
-			m.setCurrent(a);
-			this.label = m.readLabel();
-		}
+	// this input is necessary a training input
+	public Input(int a) throws IOException{
+		MnistManager m = new MnistManager("src/train-images.idx3-ubyte","src/train-labels.idx1-ubyte");
+		m.setCurrent(a);
+		this.label = m.readLabel();
+	}
 	
+	// input for whether training or testing
+	public Input(int a, boolean isTraining) throws IOException{
 		
+		MnistManager m;
+		if(isTraining){m = new MnistManager("src/train-images.idx3-ubyte","src/train-labels.idx1-ubyte");}
+		else {m = new MnistManager("src/t10k-images.idx3-ubyte","src/t10k-labels.idx1-ubyte");}
+		m.setCurrent(a);
+		this.label = m.readLabel();
+	} 
+		
+	
 	//Methods
 	public int getLabel(){
 		return this.label;
