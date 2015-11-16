969eb8cc68cb48c3c82389ad5c1fa9237001b7e4
Guriido <christian.hamelain@laposte.net>
2015-11-16 14:32:03 +0100

    added input set

--- src/TestXOR.java
+++ src/TestXOR.java
@@ -1,4 +1,31 @@
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
 public class TestXOR {
 
+	public static void main(String[] args) {
+		
+		
+		//Input DataBase creation
+		
+		int[] valuesA = {1, 1};
+		Input a = new Input(valuesA, 0);
+		int[] valuesB = {1, 0};
+		Input b = new Input(valuesB, 1);
+		int[] valuesC = {0, 1};
+		Input c = new Input(valuesC, 1);
+		int[] valuesD = {0, 0};
+		Input d = new Input(valuesD, 0);
+		
+		Input[] inputs = {a, b, c, d};
+		
+
+		
+	
+	}
 }
