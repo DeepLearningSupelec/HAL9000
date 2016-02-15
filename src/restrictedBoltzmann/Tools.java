4b80b10b30704ad60d38b23f32103361181d9107
PierreYvesH <py.hasan@gmail.com>
2016-02-15 22:18:38 +0100

    Tools Java doc

--- src/restrictedBoltzmann/Tools.java
+++ src/restrictedBoltzmann/Tools.java
@@ -2,6 +2,20 @@ package restrictedBoltzmann;
 
 public class Tools {
 
+	/**
+	 * <p>image1Dto2D</p>
+	 * <p>Changes a one dimension table of double into a two dimension table with values that can produce a grey-scale image </p>   
+	 * 
+	 * 
+	 * 
+	 * @param image1D
+	 * 			The table of double to transform
+	 * @param xSize
+	 * 			Size of x dimension in pixel	
+	 * @param ySize
+	 * 			Size of y dimension in pixel
+	 * @return
+	 */
 	
 	
 	static public int[][] image1Dto2D(double[] image1D, int xSize, int ySize){
@@ -15,6 +29,20 @@ public class Tools {
 		return image2D;
 	}
 	
+	/**
+	 * <p>image1Dto2D</p>
+	 * <p>Changes a one dimension table of int into a two dimension table with values that can produce a grey-scale image </p>   
+	 * 
+	 * 
+	 * 
+	 * @param image1D
+	 * 			The table of int to transform
+	 * @param xSize
+	 * 			Size of x dimension in pixel	
+	 * @param ySize
+	 * 			Size of y dimension in pixel
+	 * @return
+	 */
 	static public int[][] image1Dto2D(int[] image1D, int xSize, int ySize){
 		//int size = (int)(Math.sqrt(image1D.length));
 		int[][] image2D = new int[xSize][ySize];
