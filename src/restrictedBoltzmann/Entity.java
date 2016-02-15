0e5d6886874e62a476192cec6e84b452f1db30d2
fTrestour <fabien.trestour@supelec.fr>
2016-02-15 11:33:37 +0100

    Adding the BMP class tu generate bitmaps

--- src/restrictedBoltzmann/Entity.java
+++ src/restrictedBoltzmann/Entity.java
@@ -1,26 +1,24 @@
 package restrictedBoltzmann;
 
+/**
+ * <p> This class represents a single Boltzmann neuron.</p>
+ * <p>Its characteristics are:
+ * <ul>
+ * 	<li>
+ * 		Its state : it represents the <b>output</b> of the neuron. It can take any integer value.
+ * 	</li>
+ * 	<li>
+ * 		Its <b>bias</b>. It can take any float value.
+ * 	</li>
+ * 	<li>
+ * 		Its <b>id</b>. It's an integer that can not be mofified.
+ * 	</li>
+ * </ul>
+ * <p>
+ * 
+ * @see RestrictedBoltzmannMachine
+ */
 public class Entity {
-	
-	/**
-     * <p> This class represents a single Boltzmann neuron.</p>
-     * <p>Its characteristics are:
-     * <ul>
-     * 	<li>
-     * 		Its state : it represents the <b>output</b> of the neuron. It can take any integer value.
-     * 	</li>
-     * 	<li>
-     * 		Its <b>bias</b>. It can take any float value.
-     * 	</li>
-     * 	<li>
-     * 		Its <b>id</b>. It's an integer that can not be mofified.
-     * 	</li>
-     * </ul>
-     * <p>
-     * 
-     * @see RestrictedBoltzmannMachine
-     * 
-     */
 	
 	
 	//Attributes
@@ -35,7 +33,7 @@ public class Entity {
 	//Constructor
 	
 	/**
-     * <b>Entity constructor.</b>
+     * <p>Entity constructor.</p>
      * <p>
      * 	The state and biais are set to 0.
      * </p>
@@ -53,7 +51,7 @@ public class Entity {
 	}
 	
 	/**
-     * <b>Entity constructor.</b>
+     * <p>Entity constructor.<p>
      * <p>
      * 	The state is set to 0.
      * </p>
@@ -77,33 +75,55 @@ public class Entity {
 	//Methods
 	
 	/**
-     * <b>Allows to get the value of the state</b>
-     * <p>
-     * 	The state and biais are set to 0.
-     * </p>
-     * 
+     * <p>Allows to get the value of the state</p>
      * @see Entity#state
      */
 	public int getState(){
 		return this.state;
 	}
 	
+	/**
+     * <p>Allows to set the value of the state to s</p>
+     * @param s
+     * 		The new value of the neuron state
+     * @see Entity#state
+     */
 	public void setState(int s){
 		this.state = s;
 	}
 	
+	/**
+     * <p>Allows to get the id of the neuron</p>
+     * @see Entity#neuron
+     */
 	public int getId(){
 		return this.id;
 	}
 	
+	/**
+     * <p>Allows to set the value of the bias to b</p>
+     * @param b
+     * 		The new value of the biais
+     * @see Entity#biais
+     */
 	public void setBias(double b){
 		this.bias = b;
 	}
 	
+	/**
+     * <p>Allows to get the value of the bias</p>
+     * @see Entity#biais
+     */
 	public double getBias(){
 		return this.bias;
 	}
 	
+	/**
+     * <p>???</p>
+     * @param e
+     * 		???
+     * @see Entity#entityDiff
+     */
 	public void setEntityDiff(double e){
 		this.entityDiff = e;
 	}
@@ -111,7 +131,6 @@ public class Entity {
 	public double getEntityDiff(){
 		return this.entityDiff;
 	}
-	
 	
 }
 
