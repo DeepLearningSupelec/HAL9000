a2e0425c1a93a44aa093f60ad0bfbf479d81d161
ftrestour <fabien.trestour@supelec.fr>
2016-04-22 11:44:03 +0200

    Test OpenCV OK

--- src/TestsOpenCV.java
+++ src/TestsOpenCV.java
@@ -1,30 +1,29 @@
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
 import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.videoio.VideoWriter;
 
 public class TestsOpenCV
 {
    public static void main( String[] args )
    {	
-	  int dim = 200;
-	   
-      System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
-      Mat mat = Mat.eye( dim, dim, CvType.CV_8UC1 );
-      System.out.println( "mat = " + mat.dump() );
+	  System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+	  
+	  Mat img = Imgcodecs.imread("D:\test.png");
+	  if(img==null){
+		  System.out.println("Fuck!-1");
+	  }
+	  int dim = img.dims();
       Size size = new Size(dim,dim);
       
       VideoWriter vid = new VideoWriter();
-      vid.open("D:/MyVideo.avi", -1, 1, size, false);
+      vid.open("D:\\MyVideo.avi", -1, 60, size, true);
       System.out.println("Vidéo créée");
       
-      Mat img = imread("D:\test.png");
-      
-      for(int i=0; i<60; i++){
+      for(int i=0; i<120; i++){
+    	  img = Imgcodecs.imread("D:\\test.png");
     	  vid.write(img);
-    	  System.out.println("Image "+i);
       }
       
       vid.release();
@@ -34,11 +33,6 @@ public class TestsOpenCV
       /*fourcc = choix du codec. quel code pour h264?*/
       /*http://docs.opencv.org/java/3.1.0/org/opencv/videoio/VideoWriter.html#write-org.opencv.core.Mat-*/
      /* //Path p = Paths.get(System.getProperty("user.home"),"Desktop", "im0" + i + ".png");
-      V
-      if ( !vid.isOpened() ) //if not initialize the VideoWriter successfully, exit the program
-      {
-           System.out.println("fuck");
-      }
 
 
       */
